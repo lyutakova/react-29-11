e959ef577ffe0a11150b22fbe0fabe7d99e31ac7
tetiana <lyutakova@gmail.com>
2023-02-24 19:40:18 +0100

    added id and something else

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+// const title = React.createElement('h1', null, 'Hello react))')
+const title = React.createElement(
+    'h1',
+    {
+        id: 'title',
+        className: 'red',
+    },
+    'Hello react))'
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(<React.StrictMode>{title}</React.StrictMode>)
