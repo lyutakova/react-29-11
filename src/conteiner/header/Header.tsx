9d90246cab3cf14ef4437fcb1e92e7331a6a3dca
tetiana <lyutakova@gmail.com>
2023-03-06 18:19:33 +0100

    List of products

--- src/conteiner/header/Header.tsx
+++ src/conteiner/header/Header.tsx
@@ -4,7 +4,7 @@ import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
-import menu from 'components/logo/menu/menu'
+import menu from 'components/menu/menu'
 import CartHeader from 'components/CartHeader.tsx/CartHeader'
 
 type Props = {}
