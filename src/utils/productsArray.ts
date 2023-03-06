42b54dd9b2d6184569c56e5157cd6071983e0b1d
tetiana <lyutakova@gmail.com>
2023-03-06 19:49:01 +0100

    products massives in file productsArray.ts

---
+++ src/utils/productsArray.ts
@@ -0,0 +1,64 @@
+type Product = {
+    title: string
+    description: string
+    type:string
+    capacity:string
+    price:number
+}
+
+const productsArray: Product[] = [
+    {
+    title: 'iPhone 14 Pro',
+    description:'This is ghgjjg 14',
+    type:' phone',
+    capacity: '128',
+    price: 1500,
+    },
+
+    {
+        title: 'iPhone 13 Pro',
+        description:'This is ghgjjg 14',
+        type:' phone',
+        capacity: '128',
+        price: 1400,
+        },
+
+        {
+            title: 'iPhone 12 Pro',
+            description:'This is ghgjjg 14',
+            type:' phone',
+            capacity: '254',
+            price: 1200,
+            },
+
+            {
+                title: 'iPhone 13 Pro',
+                description:'This is ghgjjg 14',
+                type:' phone',
+                capacity: '512',
+                price: 1500,
+                },
+
+                {
+                    title: 'iPhone 11 Pro',
+                    description:'This is ghgjjg 14',
+                    type:' phone',
+                    capacity: '64',
+                    price: 690,
+                    },
+
+{
+                        title: 'iPhone 14 Pro',
+                        description:'This is ghgjjg 14',
+                        type:' phone',
+                        capacity: '128',
+                        price: 990,
+                        },
+
+    
+
+    
+
+]
+
+export default productsArray
